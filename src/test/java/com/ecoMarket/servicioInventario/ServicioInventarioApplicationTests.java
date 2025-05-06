78a27694f473143beb94f0e8e14e29d6c55d0f20
jival <joi.valenzuela@duocuc.cl>
2025-05-06 17:53:56 -0400

    Squashed 'inventario/' content from commit 5882b0d
    git-subtree-dir: inventario
    git-subtree-split: 5882b0d070791b1d90be9014f94be16b49a39b0d

---
+++ src/test/java/com/ecoMarket/servicioInventario/ServicioInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoMarket.servicioInventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
