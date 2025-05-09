4fb46aea70a25f412a3aaec87cd2a60ae5b2615f
Melipilla <melipilla@duoc.acad>
2025-05-09 17:02:31 -0400

    solo para subir la caarpeta

---
+++ servicioOrden/src/test/java/com/ecoMarket/servicioOrden/ServicioOrdenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoMarket.servicioOrden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioOrdenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
