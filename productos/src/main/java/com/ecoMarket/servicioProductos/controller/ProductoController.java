220003c8a15b3f140324f9cbcbdbf09bd5752c3e
jival <joi.valenzuela@duocuc.cl>
2025-06-12 22:39:09 -0400

    Realizada configuracion de Swagger, Realizada documentacion de metodo listar() de la API de productos

--- productos/src/main/java/com/ecoMarket/servicioProductos/controller/ProductoController.java
+++ productos/src/main/java/com/ecoMarket/servicioProductos/controller/ProductoController.java
@@ -2,6 +2,15 @@ package com.ecoMarket.servicioProductos.controller;
 
 import com.ecoMarket.servicioProductos.model.Producto;
 import com.ecoMarket.servicioProductos.service.ProductoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,16 +20,70 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/productos")
+@Tag(name="Productos", description = "Operaciones relacionadas con los productos")
 public class ProductoController {
 
     @Autowired
     private ProductoService productoService;
 
-    @GetMapping()
-    public ResponseEntity<List<Producto>> listar(
-            @RequestParam(required = false) String categoria,
-            @RequestParam(required = false) String marca) {
 
+    @GetMapping()
+    @Operation(summary = "Obtener todos los productos",
+            description = "Obtiene una lista de todos los productos, con posibilidad de filtrar por categoria y/o marca")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Productos listados exitosamente",
+                    content = @Content(mediaType = "application/json",
+                            array = @ArraySchema
+                                    (schema = @Schema(implementation = Producto.class)),
+                            examples = @ExampleObject(
+                                    name = "Lista de Ejemplo",
+                                    description = "Ejemplo de listado de los productos, sin usar filtros",
+                                    value = """
+                                            [
+                                                {
+                                                    "id": 1,
+                                                    "prodCode": 1,
+                                                    "nombre": "Hamburguesa Vacuno 100g",
+                                                    "marca": "PF",
+                                                    "precio": 750,
+                                                    "categoria": "hamburguesas",
+                                                    "esEco": false,
+                                                    "stock": 134
+                                                },
+                                                {
+                                                    "id": 2,
+                                                    "prodCode": 2,
+                                                    "nombre": "yoghurt chirimolla",
+                                                    "marca": "Colun",
+                                                    "precio": 326,
+                                                    "categoria": "Lacteos y Quesos",
+                                                    "esEco": true,
+                                                    "stock": 456
+                                                }
+                                            ]
+                                            """
+                            ))),
+            @ApiResponse(responseCode = "204", description = "No se encontraron Productos",
+                    content = @Content(schema = @Schema(hidden = true)))
+    })
+    public ResponseEntity<List<Producto>> listar(
+            @Parameter(
+                    name = "Categoria",
+                    description = "Categoria del producto",
+                    required = false,
+                    example = "Jugos",
+                    schema = @Schema(type = "String")
+                    )
+            @RequestParam(required = false) String categoria,
+            @Parameter(
+                    name = "Marca" ,
+                    description = "Marca del producto",
+                    required = false,
+                    example = "Watts",
+                    schema = @Schema(type = "String")
+            )
+            @RequestParam(required = false) String marca)
+    {
         List<Producto> productos;
 
         categoria = "".equals(categoria) ? null : categoria;
